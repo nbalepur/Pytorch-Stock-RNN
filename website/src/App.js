e94ee74b86cb445048a58b9caacdd8688ea59093
nishantbalepur <balepur2@illinois.edu>
2021-01-03 16:02:31 -0500

    started website header

--- website/src/App.js
+++ website/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+import Header from "./components/header.js";
+
+import Brain from "./images/brain.png";
+
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    document.body.style = "background: #1a1a2e";
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <img
+          alt={"brain"}
+          src={Brain}
+          style={{ width: 250, height: 250 }}
+        ></img>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ website/src/components/header.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  state = {};
+  render() {
+    return (
+      <div style={{ backgroundColor: "#17223b" }}>
+        <div class="container">
+          <br></br>
+          <br></br>
+          <br></br>
+          <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8" align="center">
+              <h1 style={{ color: "white", fontSize: 50 }}>
+                RNN Stock Predictor
+              </h1>
+              <h4 style={{ color: "white" }}>
+                Type in your stock ticker symbol to get started!
+              </h4>
+              <br></br>
+
+              <div class="col-md-4">
+                <div class="input-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="TSLA, AAPL, etc."
+                    aria-label="TSLA, AAPL, etc."
+                    aria-describedby="basic-addon2"
+                  />
+                  <div class="input-group-append" style={{ marginLeft: 5 }}>
+                    <button type="button" class="btn btn-primary" style={{}}>
+                      Predict!
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-2"></div>
+          </div>
+          <br></br>
+          <br></br>
+          <br></br>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
